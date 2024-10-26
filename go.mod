8668a43b93ec0c61ad7e301ce5f2f869a08862b2
tracyymaii <tracyttmai@gmail.com>
2024-10-26 03:37:45 -0700

    CURRENLY DEFINES GRAMMAR, TOKENS, AND LEXEMES

--- go.mod
+++ go.mod
@@ -2,7 +2,3 @@ module cpl/parser
 
 go 1.18
 
-require (
-	github.com/elliotchance/orderedmap v1.6.0 // indirect
-	github.com/elliotchance/orderedmap/v2 v2.4.0 // indirect
-)
--- parser.go
+++ parser.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"unicode"
 )
@@ -9,6 +11,36 @@ import (
 // "bufio"
 // "os"
 // "strings"
+
+type grammar struct {
+	token  string
+	lexeme string
+}
+
+var tokenPatterns = map[string]string{
+	"ID":        `^[a-z]+`, // PROBLEM, prints, point, triangle, and squares as
+	"NUM":       `^\d+`,
+	"SEMICOLON": `^;`,
+	"COMMA":     `^,`,
+	"PERIOD":    `(^\.$)`,
+	"LPAREN":    `^\(`,
+	"RPAREN":    `^\)`,
+	"ASSIGN":    `^=`,
+}
+
+var tokenPatterns2 = map[string]string{
+	"POINT":    `^point\b`,
+	"TRIANGLE": `^triangle\b`,
+	"SQUARE":   `^square\b`,
+	"TEST":     `^test\b`,
+}
+
+func newToken(token string) *grammar {
+
+	g := grammar{token: token}
+	g.lexeme = ""
+	return &g
+}
 
 func main() {
 
@@ -32,6 +64,7 @@ func removeWhiteSpace(entireFile string) string {
 	noWhiteSpace = strings.ReplaceAll(noWhiteSpace, "\n", "")
 	noWhiteSpace = strings.ReplaceAll(noWhiteSpace, "\t", "")
 
+	print(noWhiteSpace)
 	return noWhiteSpace
 }
 
@@ -43,6 +76,9 @@ func tokenizer(entireFile string) {
 	var currRune rune
 	var tempType string
 	var currType string
+
+	compiledPatterns := compiledRegex()
+	compiledPatterns2 := compiledRegex2()
 
 	/*
 		match types: LOWER letters/apha, digit, [; , . () = ], alphanumeric
@@ -58,18 +94,67 @@ func tokenizer(entireFile string) {
 	for _, character := range entireFile {
 		currRune = character
 		currType = stringType(currRune)
-
 		curr = string(character)
 
 		if temp.String() == "" {
 			tempType = currType
-		} else if currType != tempType || tempType == "symbol" {
+		} else if tempType == "symbol" || currType != tempType {
 			//process token
+			processToken(temp.String(), compiledPatterns, compiledPatterns2)
+
 			temp.Reset()
 			tempType = currType
 		}
+
+		// fence post to get last char, and converts last rune to char
+		// if len(entireFile) > 0 {
+		// 	lastRune, _ := utf8.DecodeLastRune([]byte(entireFile))
+		// 	processToken(string(lastRune), compiledPatterns)
+		// }
 		temp.WriteString(curr)
 	}
+
+	processToken(temp.String(), compiledPatterns, compiledPatterns2)
+
+	//process period/last token
+}
+
+func processToken(passedToken string, compiledPatterns map[string]*regexp.Regexp, compiledPatterns2 map[string]*regexp.Regexp) {
+
+	for key, regex := range compiledPatterns2 {
+		if regex.MatchString(passedToken) {
+			fmt.Println(key + " " + passedToken)
+			return
+		}
+		// else panic(err)
+	}
+
+	for key, regex := range compiledPatterns {
+		if regex.MatchString(passedToken) {
+			fmt.Println(key + " " + passedToken)
+
+		}
+		// else panic(err)
+	}
+
+}
+
+func compiledRegex() map[string]*regexp.Regexp {
+	compiledPatterns := make(map[string]*regexp.Regexp)
+	for key, pattern := range tokenPatterns {
+		compiledPatterns[key] = regexp.MustCompile(pattern)
+	}
+
+	return compiledPatterns
+}
+
+func compiledRegex2() map[string]*regexp.Regexp {
+	compiledPatterns2 := make(map[string]*regexp.Regexp)
+	for key, pattern := range tokenPatterns2 {
+		compiledPatterns2[key] = regexp.MustCompile(pattern)
+	}
+
+	return compiledPatterns2
 }
 
 func stringType(char rune) string {
