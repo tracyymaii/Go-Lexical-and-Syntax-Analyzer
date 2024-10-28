cf50dd5129a416f710c116a878e7ebc3662f47d7
tracyymaii <tracyttmai@gmail.com>
2024-10-27 23:51:54 -0700

    donezo

--- parser.go
+++ parser.go
@@ -1,5 +1,12 @@
 package main
 
+/*
+Title : Go Parser
+Purpose: To search for lexicode and syntax errors based on given grammar
+Author: Tracy Mai
+Date: October 27, 2024
+*/
+
 import (
 	"fmt"
 	"os"
@@ -13,8 +20,17 @@ type grammar struct {
 	lexeme string
 }
 
+type data struct {
+	x string
+	y string
+}
+
+var dataPoints = make(map[string]data)
+
+var shape bool
+
 var tokenPatterns = map[string]string{
-	"ID":        `^[a-z]+`, // PROBLEM, prints, point, triangle, and squares as
+	"ID":        `^[a-z]+`,
 	"NUM":       `^\d+`,
 	"SEMICOLON": `^;`,
 	"COMMA":     `^,`,
@@ -31,131 +47,124 @@ var tokenPatterns2 = map[string]string{
 	"TEST":     `^test\b`,
 }
 
-var collectedTokens []grammar
+var stringGrammar = map[string]string{
+	"ID":        "lowercase letters a-z",
+	"NUM":       "numbers 0-9",
+	"SEMICOLON": ";",
+	"COMMA":     ",",
+	"PERIOD":    ".",
+	"LPAREN":    "(",
+	"RPAREN":    ")",
+	"ASSIGN":    "=",
+	"POINT":     "point",
+	"TRIANGLE":  "triangle",
+	"SQUARE":    "square",
+	"TEST":      "test",
+}
 
-// func newToken(token string) *grammar {
+var collectedTokens = make([]grammar, 0)
 
-// 	g := grammar{token: token}
-// 	g.lexeme = ""
-// 	return &g
-// }
+var idPatterns = []string{
+	"ID", "ASSIGN", "POINT", "LPAREN", "NUM", "COMMA", "NUM", "RPAREN",
+}
+
+var testPattern = []string{
+	"TEST", "LPAREN",
+}
+
+var trianglePattern = []string{
+	"TRIANGLE", "COMMA", "ID", "COMMA", "ID", "COMMA", "ID", "RPAREN",
+}
+
+var squarePattern = []string{
+	"SQUARE", "COMMA", "ID", "COMMA", "ID", "COMMA", "ID", "COMMA", "ID", "RPAREN",
+}
+
+var triangleNames = []string{
+	"line", "vertical", "horizontal", "equilateral", "isosceles", "right", "scalene", "obtuse",
+}
+
+/*
+Main
+Combines all the methods of the program to correctly function
+@param none
+@return none
+*/
 
 func main() {
 
-	/*
-
-				prompt>go run . input.txt -s
-				; Processing Input File input.txt
-				; Lexical and Syntax analysis passed
-				; Generating Scheme Code
-				(process-triangle (make-point 2 3) (make-point 1 4) (make-point 3 4))
-				prompt>
-
-
-
-		func compiledRegex2() map[string]*regexp.Regexp {
-			compiledPatterns2 := make(map[string]*regexp.Regexp)
-			for key, pattern := range tokenPatterns2 {
-				compiledPatterns2[key] = regexp.MustCompile(pattern)
-			}
-
-			return compiledPatterns2
-		}
-
-		if regex.MatchString(passedToken) {
-					fmt.Println(key + " " + passedToken)
-					collectedTokens
-
-	*/
-
 	var i, j string
 
-	fmt.Print("prompt>go run .")
+	fmt.Print("prompt> go run . ")
 	fmt.Scan(&i, &j)
 
-	filepattern := regexp.MustCompile(`/^[a-z0-9]+\.[a-z]+$\b/gm`)
+	filepattern := regexp.MustCompile(`^[a-z0-9]+\.[a-z]+$`)
 
 	if i == "" {
 		errorMessage := `Missing parameter, file:
-		go run . filename - flag
-		flag can be p for prolog generation
-		flag can be s for prolog generation`
-
+go run . filename - flag
+flag can be p for Prolog generation
+flag can be s for Scheme generation`
 		fmt.Printf("%s\n", errorMessage)
 		return
 	} else if j == "" {
 		errorMessage := `Missing parameter, usage:
-		go run . filename - flag
-		flag can be p for prolog generation
-		flag can be s for prolog generation`
-
+go run . filename - flag
+flag can be p for Prolog generation
+flag can be s for Scheme generation`
 		fmt.Printf("%s\n", errorMessage)
 		return
-	} else if !filepattern.MatchString(i) || (strings.ToLower(j) != "-s" || strings.ToLower(j) != "-p") {
+	} else if !filepattern.MatchString(i) || (strings.ToLower(j) != "-s" && strings.ToLower(j) != "-p") {
 		errorMessage := `Incorrect syntax for filename and flag:
-		go run . filename - flag
-		flag can be p for prolog generation
-		flag can be s for prolog generation`
-
+go run . filename - flag
+flag can be p for Prolog generation
+flag can be s for Scheme generation`
 		fmt.Printf("%s\n", errorMessage)
 		return
 	}
 
-	if strings.ToLower(j) == "-s" {
-		//checking for fails in code
-
-		// assume should be good by now
-
-		scheme()
-	} else {
-		// its accepted by p
-		print("hello")
-	}
-
-	// i should be a file name, j needs to be -s or -p
-
-	// need to write if input wrong, to give t
-
-	// check, if not fail
-
-	// need to move this to after an actual things, aka where we check for lexical and synteax error
-	inputSuccess := `; Processing Input File input.txt
-		; Lexical and Syntax analysis passed
-		; Generating Scheme Code
-		(process-triangle (make-point 2 3) (make-point 1 4) (make-point 3 4))
-		prompt>`
-
-	fmt.Printf("%s\n", inputSuccess)
-
-	// opens and reads file
-	file, err := os.ReadFile("test1.cpl")
+	fileData, err := os.ReadFile(i)
 	if err != nil {
-		// print out why error occurs
-		panic(err)
+		fmt.Println("Error reading file:", err)
+		return
 	}
 
-	// turns file into a string
-	fileString := string(file)
+	fileString := string(fileData)
 
-	// removes whitespace, new line, and tabs from the file then
-	// sends the clean string file the tokenizer
 	tokenizer(removeWhiteSpace(fileString))
 
-	fmt.Println(collectedTokens)
+	checkSyntax()
+	organizeVariables()
+
+	if strings.ToLower(j) == "-s" {
+		scheme()
+	} else if strings.ToLower(j) == "-p" {
+		prolog()
+	}
 }
 
+/*
+Removes White Space
+Removes the whitespace, newlines, and tabs from the file
+@param the file
+@return the file without whitespace
+*/
 func removeWhiteSpace(entireFile string) string {
 	noWhiteSpace := strings.ReplaceAll(entireFile, " ", "")
 	noWhiteSpace = strings.ReplaceAll(noWhiteSpace, "\n", "")
 	noWhiteSpace = strings.ReplaceAll(noWhiteSpace, "\t", "")
 
-	print(noWhiteSpace)
 	return noWhiteSpace
 }
 
-func tokenizer(entireFile string) { // does not yet reject cap letters
+/*
+Tokenizer
+Splits up the file into tokens and uses Process Tokens to check if it matches the grammar
+@param the file
+@return none
+*/
+func tokenizer(entireFile string) {
 
-	//  holds the temp string to tokenize
 	var temp strings.Builder
 	var curr string
 	var currRune rune
@@ -165,17 +174,6 @@ func tokenizer(entireFile string) { // does not yet reject cap letters
 	compiledPatterns := compiledRegex()
 	compiledPatterns2 := compiledRegex2()
 
-	/*
-		match types: LOWER letters/apha, digit, [; , . () = ], alphanumeric
-
-		2 step process: 1. we jsut put it in to the word if its the same
-						2. THEN we judge the word and kick it out if its not in the club
-	*/
-
-	// parses through the file
-	// if string is empty, j add letter, and move onto the next
-	// scan letter, if letter does not match type of temp string or its a symbol, process temp string
-	// always append char
 	for _, character := range entireFile {
 		currRune = character
 		currType = stringType(currRune)
@@ -184,77 +182,244 @@ func tokenizer(entireFile string) { // does not yet reject cap letters
 		if temp.String() == "" {
 			tempType = currType
 		} else if tempType == "symbol" || currType != tempType {
-			//process token
 			processToken(temp.String(), compiledPatterns, compiledPatterns2)
 
 			temp.Reset()
 			tempType = currType
 		}
-
-		// fence post to get last char, and converts last rune to char
-		// if len(entireFile) > 0 {
-		// 	lastRune, _ := utf8.DecodeLastRune([]byte(entireFile))
-		// 	processToken(string(lastRune), compiledPatterns)
-		// }
 		temp.WriteString(curr)
 	}
 
 	processToken(temp.String(), compiledPatterns, compiledPatterns2)
-
-	//process period/last token
 }
 
 /*
-func newToken(token string) *grammar {
-
-	g := grammar{token: token}
-	g.lexeme = ""
-	return &g
-}
-
-type grammar struct {
-	token  string
-	lexeme string
-}
+Process Token
+Process tokens and checks to see if they exist in the grammar
+Exits the program, if a token is not in the grammar
+@param the token, compiled maps of the regex patterns
+@return none
 */
-
 func processToken(passedToken string, compiledPatterns map[string]*regexp.Regexp, compiledPatterns2 map[string]*regexp.Regexp) {
 
 	for key, regex := range compiledPatterns2 {
 		if regex.MatchString(passedToken) {
-			fmt.Println(key + " " + passedToken)
 			collectedTokens = append(collectedTokens, grammar{token: key, lexeme: passedToken})
 			return
 		}
-		// else panic(err)
 	}
 
 	for key, regex := range compiledPatterns {
 		if regex.MatchString(passedToken) {
-			fmt.Println(key + " " + passedToken)
-
-			if key == "ID" || key == "NUM" {
-				collectedTokens = append(collectedTokens, grammar{token: key, lexeme: passedToken})
-			} else {
-				collectedTokens = append(collectedTokens, grammar{token: key})
-			}
-
+			collectedTokens = append(collectedTokens, grammar{token: key, lexeme: passedToken})
+			return
 		}
-		// else panic(err)
 	}
 
+	fmt.Println("Lexicode error " + passedToken + " not recognized")
+	os.Exit(0)
 }
 
-func scheme() {
-	output := `; Processing Input File input.txt
-	; Lexical and Syntax analysis passed
-	; Generating Scheme Code`
+/*
+Check Syntax
+Uses Match Syntax to check the order of the tokens to our expected format
+@param none
+@return none
+*/
+func checkSyntax() {
 
-	fmt.Println("(process-")
+	for i := 0; i < len(collectedTokens); i++ {
+
+		switch collectedTokens[i].token {
+		case "ID":
+			if !shape {
+				i = matchSyntax(idPatterns, i)
+			}
+		case "TEST":
+			i = matchSyntax(testPattern, i)
+		case "TRIANGLE":
+			shape = true
+			i = matchSyntax(trianglePattern, i)
+
+		case "SQUARE":
+			shape = true
+			i = matchSyntax(squarePattern, i)
+		}
+	}
+}
+
+/*
+Match Syntax
+Uses check the order of the tokens to our expected format
+Exits if there is a syntax error
+@param the pattern we are matching, index of the current array
+@return none
+*/
+func matchSyntax(pattern []string, index int) int {
+
+	var i int
+
+	for i := 0; i < len(pattern); i++ {
+		if collectedTokens[index+i].token == pattern[i] {
+			continue
+		} else {
+			fmt.Println("Syntax error " + collectedTokens[index+i].lexeme + " found " + pattern[i] + " expected")
+			os.Exit(0)
+		}
+	}
+
+	if collectedTokens[i+index+1].token == "SEMICOLON" && i+index+1 == len(collectedTokens)-1 {
+		fmt.Println("Syntax error ; found . expected")
+		os.Exit(0)
+	}
+	if collectedTokens[i+index+1].token == "PERIOD" && i+index+1 != len(collectedTokens)-1 {
+		fmt.Println("Syntax error . found ; expected")
+		os.Exit(0)
+	}
+
+	return i + index + 1
+}
+
+/*
+Organize Variables
+Groups the variables and their x and y values into structs
+@param none
+@return none
+*/
+func organizeVariables() {
+
+	var id string
+	var xVal string
+	var yVal string
+
+	for _, items := range collectedTokens {
+		if items.token == "ID" {
+			id = items.lexeme
+		} else if items.token == "NUM" && xVal == "" {
+			xVal = items.lexeme
+		} else if items.token == "NUM" {
+			yVal = items.lexeme
+		}
+
+		if id != "" && xVal != "" && yVal != "" {
+			dataPoints[id] = data{x: xVal, y: yVal}
+			id = ""
+			xVal = ""
+			yVal = ""
+		}
+
+	}
+}
+
+/*
+Scheme
+Runs the scheme output
+@param none
+@return none
+*/
+func scheme() {
+
+	shapeSeen := false
+
+	output := "\n; processing Input File input.txt \n; Lexical and Syntax analysis passed\n; Generating Scheme Code"
+
+	for i := 0; i < len(collectedTokens); i++ {
+		if collectedTokens[i].token == "TRIANGLE" || collectedTokens[i].token == "SQUARE" {
+			shapeSeen = true
+			output += "\n(process-" + collectedTokens[i].lexeme
+		} else if collectedTokens[i].token == "ID" && shapeSeen == true {
+			pointData, exists := dataPoints[collectedTokens[i].lexeme]
+			if exists {
+				output += " (make-point " + pointData.x + " " + pointData.y + ")"
+			} else {
+				fmt.Println("Attempted to use a data point that does not exist")
+				os.Exit(0)
+			}
+		} else if collectedTokens[i].token == "TEST" {
+			output += ")"
+			shapeSeen = false
+		}
+	}
+	output += ")"
 
 	fmt.Printf("%s\n", output)
 }
 
+/*
+Prolog
+Runs the prolog output
+@param none
+@return none
+*/
+func prolog() {
+	shapeSeen := false
+
+	output := "\n/* processing input file input.txt \n   Lexical and Syntax analysis passed \n   Generating Prolog Code */\n"
+	squareOutput := ""
+
+	for i := 0; i < len(collectedTokens); i++ {
+		if collectedTokens[i].token == "TRIANGLE" || collectedTokens[i].token == "SQUARE" {
+			shapeSeen = true
+			output += "\n/* Processing test(" + collectedTokens[i].lexeme
+			squareOutput += "\nquery(" + collectedTokens[i].lexeme + "("
+
+		} else if collectedTokens[i].token == "ID" && shapeSeen == true {
+			pointData, exists := dataPoints[collectedTokens[i].lexeme]
+			if exists {
+
+				output += ", " + collectedTokens[i].lexeme
+
+				squareOutput += "point2d(" + pointData.x + ", " + pointData.y + "),"
+
+			} else {
+				fmt.Println("Attempted to use a data point that does not exist")
+				os.Exit(0)
+			}
+		} else if collectedTokens[i].token == "TEST" || collectedTokens[i].token == "PERIOD" {
+			if shapeSeen {
+				output += ") */"
+			}
+			if len(squareOutput) > 0 {
+				squareOutput = squareOutput[:len(squareOutput)-1]
+			}
+			if shapeSeen {
+				squareOutput += "))."
+			}
+			shapeSeen = false
+
+			fmt.Printf("%s\n", output)
+			fmt.Printf("%s\n", squareOutput)
+
+			lines := strings.Split(squareOutput, "\n")
+
+			var filteredLines []string
+
+			for _, line := range lines {
+				if strings.Contains(line, "triangle") {
+					filteredLines = append(filteredLines, line)
+				}
+			}
+
+			for j := 0; j < len(filteredLines); j++ {
+				temp := filteredLines[j]
+				for i := 0; i < len(triangleNames); i++ {
+					squareOutput = strings.Replace(squareOutput, "triangle", triangleNames[i], 1)
+					fmt.Printf("%s\n", squareOutput)
+					squareOutput = temp
+				}
+			}
+		}
+	}
+
+	fmt.Println("\n/* Query Processing */\nwriteln(T) :- write(T), nl.\nmain:- forall(query(Q), Q-> (writeln(‘yes’)) ; (writeln(‘no’))),\n\thalt.")
+}
+
+/*
+Compiled Regex
+Compiles the regex patterns for tokenPatterns
+@param none
+@return none
+*/
 func compiledRegex() map[string]*regexp.Regexp {
 	compiledPatterns := make(map[string]*regexp.Regexp)
 	for key, pattern := range tokenPatterns {
@@ -264,6 +429,12 @@ func compiledRegex() map[string]*regexp.Regexp {
 	return compiledPatterns
 }
 
+/*
+Compiled Regex
+Compiles the regex patterns for tokenPatterns2
+@param none
+@return none
+*/
 func compiledRegex2() map[string]*regexp.Regexp {
 	compiledPatterns2 := make(map[string]*regexp.Regexp)
 	for key, pattern := range tokenPatterns2 {
@@ -273,6 +444,12 @@ func compiledRegex2() map[string]*regexp.Regexp {
 	return compiledPatterns2
 }
 
+/*
+stringType
+Converts runes to string and determines if they're a digit, letter, or special character
+@param rune
+@return string
+*/
 func stringType(char rune) string {
 
 	var charType string
